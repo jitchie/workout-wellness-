3513f45fd42b94f6dd3d2f15cff18bf2b7cdb41e
Jack Ritchie <jwf.ritchie@gmail.com>
2021-07-02 14:58:18 +1000

    connection is listening on port 3000

--- Develop/server.js
+++ Develop/server.js
@@ -1,17 +1,30 @@
+//Aquire all the npm packages.
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require('path');
 
+//create a port to listen on
 const PORT = process.env.PORT || 3000;
 
 const User = require("../Develop/public/model/model.js");
+
+//assign express to a variable for ease to use methods.
 const app = express();
 
-app.use(logger("dev"));
 
+//app.use middleware for POST and GET data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", { useNewUrlParser: true });
+//create a connection instance 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
+    useNewUrlParser: true, 
+    useFindAndModify: false
+});
+
+//creates an event listener and feedback on connection status in node shell.
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
