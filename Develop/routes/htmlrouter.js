d240d2c4f9636acd828ff79d14ce106f0bf19fc1
Jack Ritchie <jwf.ritchie@gmail.com>
2021-07-05 19:22:47 +1000

    updated html routes, still getting middleware error

--- Develop/routes/htmlrouter.js
+++ Develop/routes/htmlrouter.js
@@ -1,4 +1,12 @@
-const express = require('express');
+const express = require('express').router();
 const path = require('path');
+
+router.get("/excercise", (req, res) =>{
+    res.sendFile(path.join(__dirname, "../public/excercise.html"))
+});
+
+router.get("/stats", (req,res)=>{
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
+});
 
 module.exports = router;
